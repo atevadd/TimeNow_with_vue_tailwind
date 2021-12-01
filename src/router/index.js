83ebd886ae785d613ad46b0265aba0c58a0dc3f2
Adeniyi Tofunmi <Adeniyitofunmi@gmail.com>
2021-12-01 13:38:56 +0100

    added page title dynamically

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Pricing.vue'),
     meta:{
-      title: "Timenow - Prcing"
+      title: "Timenow - Pricing"
     }
   }
 ]
